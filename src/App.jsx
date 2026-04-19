7028c048801583a4c245b0a34a83e6960700bae8
Gera Yeremin <gerayeremin@gmail.com>
2026-04-18 21:44:45 -0700

    Update App.jsx logo pointer

--- src/App.jsx
+++ src/App.jsx
@@ -194,7 +194,7 @@ export default function App() {
           <a className="brand" href="#top" aria-label="Sacred Grove Coaching home">
             <img
               className="brand-logo"
-              src={`${assetBase}sg-logo-highres.png`}
+              src={`${assetBase}sg-logo-final-highres.png`}
               alt="Sacred Grove Coaching"
             />
           </a>
